6793c4ba5107619674f7139618f9bac38f9af3a6
0xKyran <kyran@baarda.xyz>
2024-07-27 00:47:39 +0200

    add Umami tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,5 +2,9 @@
     import "./styles.css";
   </script>
   
+<svelte:head>
+  <script defer src="https://cloud.umami.is/script.js" data-website-id="c4876c8d-9c34-4dc1-a987-1da88e8340ba"></script>
+</svelte:head>
+  
   <slot />
   
